6b55e119a8f285d5a3d96553456d3809f47af9af
Joelton Matos <joeltonmatos@gmail.com>
2025-04-09 14:29:28 -0300

    fix(app): smooth scroll behavior to avoid scroll freezing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AppContainer } from "./App.styles";
 import Header from "./components/Header/Header";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import AppRoutes from "./routes/routes";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <AppContainer>
         <AppRoutes />
